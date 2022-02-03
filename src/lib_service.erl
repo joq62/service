f9993ef12cddf5899bd44a423a9a5a6cc3877d74
joq62 <joakim.leche@gmail.com>
2022-02-03 22:56:47 +0100

    10000->30*1000

--- src/lib_service.erl
+++ src/lib_service.erl
@@ -46,15 +46,15 @@ start_template([],_LoaderVm,StartRes)->
 	    {error,ErrorList}
     end;
 start_template([{App,Vsn}|T],LoaderVm,Acc)->
-    Result=case rpc:call(LoaderVm,loader,load_appl,[App,node()],10000) of
+    Result=case rpc:call(LoaderVm,loader,load_appl,[App,node()],30*1000) of
 	       {error,Reason}->
 		   {error,[Reason,App,Vsn]};
 	       ok->
-		   case rpc:call(LoaderVm,loader,start_appl,[App,node()],10000) of
+		   case rpc:call(LoaderVm,loader,start_appl,[App,node()],30*1000) of
 		        {error,Reason}->
 			   {error,[Reason,App,Vsn]};
 		       ok->
-			   case rpc:call(node(),App,ping,[],2000) of
+			   case rpc:call(node(),App,ping,[],5*1000) of
 			       pong->
 				   {ok,[App,Vsn]};
 			       {badrpc,Reason}->
